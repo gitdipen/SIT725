bbf94920573f6009cef9c39f9970ec561b77a3b2
Dipen <48055471+gitdipen@users.noreply.github.com>
2024-11-21 21:46:24 +1100

    Test

--- Week2/Part 2 Addition of two numbers/server.js
+++ Week2/Part 2 Addition of two numbers/server.js
@@ -10,7 +10,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 //static css
 app.use(express.static(path.join(__dirname)));
 
-//for html
+//for html file
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'index.html')); 
 });
